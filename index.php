3f35acc78a5db55075b87879591d4d8ee25800de
DaichiAndoh <d-andoh.dev@outlook.jp>
2024-07-12 20:01:12 +0900

    style: CSS修正

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ for ($i = 0; $i < $chainCount; $i++) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   </head>
   <body>
-    <div class="container my-4">
+    <div class="container w-50 my-4">
       <h1 class="text-center">Restaurant Chains</h1>
       <div class="mt-4">
         <?php foreach ($restaurantChains as $restaurantChain): ?>
